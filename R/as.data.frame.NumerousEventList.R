d36d7661a5ca86ee736271a8ebc02a475fa0ac99
Brian Connelly <briandconnelly@users.noreply.github.com>
2015-09-27 15:23:07 -0700

    Added function to coerce lists of events to data frame
    - Created as.data.frame.NumerousEventList
    - Added lubridate dependency

---
+++ R/as.data.frame.NumerousEventList.R
@@ -0,0 +1,27 @@
+#' Coerce an event list to a data frame
+#'
+#' @param event_list A list of events for a Numerous metric
+#' @param ... Optional arguments (currently unused)
+#'
+#' @return A data frame
+#' @importFrom lubridate parse_date_time
+#' @export
+#'
+#' @examples
+#' library(numerous)
+#' elist <- get_events(metric_id = "TODO")
+#' edf <- as.data.frame(elist)
+#' 
+as.data.frame.NumerousEventList <- function(event_list, ...)
+{
+    # TODO better implementation for this
+    df <- as.data.frame(event_list[[1]], stringsAsFactors=FALSE)
+    for (i in seq(2, length(event_list)))
+    {
+        df <- rbind(df, as.data.frame(event_list[[i]], stringsAsFactors=FALSE))
+    }
+    df$updated <- parse_date_time(df$updated,
+                                  orders="%Y-%m-%d %H:%M:%S", tz="UTC")
+    df
+}
+
