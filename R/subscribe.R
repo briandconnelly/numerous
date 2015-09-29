4179a99c9e4a4b2c8f1b72f1b838f59c37918da3
Brian Connelly <briandconnelly@users.noreply.github.com>
2015-09-29 12:31:14 -0700

    Subscribe updates
    - Added param validation
    - Added update_subscription alias

--- R/subscribe.R
+++ R/subscribe.R
@@ -1,5 +1,6 @@
-#' Subscribe to a metric
+#' Subscribe or update a subscription to a metric
 #'
+#' @description \code{subscribe} subscribes the given user to the a metric
 #' @param metric_id The ID of the metric
 #' @param user_id The ID of the user. If one is not provided, the current user's ID is used.
 #' @param notifications Whether or not notifications are enabled (default:
@@ -7,6 +8,7 @@
 #' arguments (see below).
 #' @param ... Any additional properties for the subscription. These include:
 #' \itemize{
+#'     \item \code{notifyOnAnyChange}: Notify whenever the metric changes
 #'     \item \code{notifyWhenAbove}: Value above which a notification will be
 #'     made
 #'     \item \code{notifyWhenBelow}: Value below which a notification will be
@@ -16,6 +18,8 @@
 #'     \item \code{notifyOnComment}: Whether or not to notify when metric is
 #'     commented on
 #'     \item \code{notifyOnLike}: Whether or not to notify when metric is liked
+#'     \item \code{notifyOnFollow}: Whether or not to notify when a new user
+#'     subsribes to this metric
 #' }
 #'
 #' @return A list containing information about the subscription
@@ -34,6 +38,18 @@ subscribe <- function(metric_id, user_id=get_numerous_id(), notifications=TRUE, 
     assert_that(is.string(metric_id))
     assert_that(is.string(user_id))
     
+    valid_args <- c("notificationsEnabled", "notifyOnAnyChange",
+                    "notifyWhenAbove", "notifyWhenAboveSet",
+                    "notifyWhenBelow", "notifyWhenBelowSet",
+                    "notifyOnPercentChange", "notifyOnPercentChangeSet",
+                    "notifyOnComment", "notifyOnLike", "notifyOnError",
+                    "notifyOnFollow")
+    
+    for (arg in names(other_args))
+    {
+        if (!arg %in% valid_args) stop(sprintf("Invalid argument '%s'", arg))
+    }
+    
     other_args[['notificationsEnabled']] <- notifications
     other_args[["notifyWhenAboveSet"]] <- "notifyWhenAbove" %in% names(other_args)
     other_args[["notifyWhenBelowSet"]] <- "notifyWhenBelow" %in% names(other_args)
@@ -51,3 +67,9 @@ subscribe <- function(metric_id, user_id=get_numerous_id(), notifications=TRUE, 
     class(rval) <- c("NumerousSubscription", "list")
     rval
 }
+
+#' @description \code{update_subscription} updates the settings for a user's subscription to
+#' a metric
+#' @rdname subscribe
+#' @export
+update_subscription <- subscribe
