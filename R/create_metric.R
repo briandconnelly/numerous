b6579e3a2bf3483c871e15ee98e8fb44bc4eaf57
Brian Connelly <briandconnelly@users.noreply.github.com>
2015-10-06 19:24:57 -0700

    Validation updates to create_metric
    - Added more argument validation

--- R/create_metric.R
+++ R/create_metric.R
@@ -19,7 +19,34 @@ create_metric <- function(label, ...)
     
     other_args <- list(...)
     other_args[['label']] <- label
+    
+    valid_args <- c("label", "description", "photoId", "kind", "currencySymbol",
+                    "value", "precision", "units", "unit", "visibility",
+                    "moreURL", "sourceKey", "sourceClass", "photoTreatment",
+                    "graphingOptions")
 
+    for (arg in names(other_args))
+    {
+        if (!arg %in% valid_args) stop(sprintf("Invalid argument '%s'", arg))
+    }
+    
+    if ("kind" %in% names(other_args))
+    {
+        assert_that(other_args$kind %in% c("number", "percent", "timer",
+                                           "currency", "temperature"))
+    }
+    
+    if ("visibility" %in% names(other_args))
+    {
+        assert_that(other_args$visibility %in% c("public", "unlisted",
+                                                 "private"))
+    }
+    
+    # TODO: sourceKey 
+    # TODO: sourceClass
+    # TODO: photoTreatment - https://developer.numerousapp.com/api#metric-api-tips-tricks-photo-treatment
+    # TODO: graphingOptions - https://developer.numerousapp.com/api#metric-api-tips-tricks-graphing-options
+    
     response <- numerous_POST(path="metrics", body=other_args)
     rval <- content(response)
     class(rval) <- c("NumerousMetric", "list")
