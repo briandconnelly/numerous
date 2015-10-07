c09dba713f48f702e410692056c8431f5f739a20
Brian Connelly <briandconnelly@users.noreply.github.com>
2015-10-06 19:00:34 -0700

    Updated update_user
    - Now gets previous values, so unspecified arguments aren't erased
    - Only accepts fullName, email, and location. Others don't seem to be
      valid.

--- R/update_user.R
+++ R/update_user.R
@@ -1,7 +1,13 @@
 #' Update a user's information
 #'
 #' @param user_id The ID of the user. If one is not provided, the current user's ID is used.
-#' @param ... Additional user parameters. See \url{https://developer.numerousapp.com/api}.
+#' @param ... Additional user parameters. These include:
+#' \itemize{
+#'     \item \code{fullName}: The user’s real name
+#'     \item \code{location}: The user’s location
+#'     \item \code{email}: The user’s email address. When supplied, a
+#'     verification request will be sent to this address.
+#' }
 #'
 #' @return A list containing information about the user
 #' @importFrom assertthat assert_that is.string not_empty
@@ -21,8 +27,22 @@ update_user <- function(user_id=get_numerous_user(), ...)
     assert_that(is.string(user_id))
     assert_that(not_empty(other_args))
     
+    my_info <- get_user(user_id = user_id)
+    newinfo <- list()
+    newinfo$fullName <- my_info$fullName
+    newinfo$location <- my_info$location
+    newinfo$email <- my_info$email
+    
+    valid_args <- c("fullName", "location", "email")
+    
+    for (arg in names(other_args))
+    {
+        if (!arg %in% valid_args) stop(sprintf("Invalid argument '%s'", arg))
+        else newinfo[[arg]] <- other_args[[arg]]
+    }
+    
     response <- numerous_PUT(path=paste("users", user_id, sep='/'),
-                             body=other_args)
+                             body=newinfo)
     
     rval <- content(response)
     class(rval) <- c("NumerousUser", "list")
